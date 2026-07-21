8556eebfc08c78437e57dc324f25c2bd23247b2c
Joe <jsschlosser@email.arizona.edu>
2026-07-21 00:09:56 -0400

    ' 	modified:   L1_file_save.py

--- L1_file_save.py
+++ L1_file_save.py
@@ -12,25 +12,25 @@ import nc_write
 import NMEA_decode
 
 def run():
-    """
-    Copies an existing NetCDF file to a new location, adds a new dimension 
-    and variable, and updates global attributes.
+	"""
+	Copies an existing NetCDF file to a new location, adds a new dimension 
+	and variable, and updates global attributes.
 
-    :param source_path: The absolute or relative path to the original .nc file.
-    :type source_path: str
-    :param target_path: The destination path where the modified .nc file will be saved.
-    :type target_path: str
-    :raises IOError: If the source file cannot be found or read.
-    :raises OSError: If there is a permissions issue or netCDF modification failure.
-    """
+	:param source_path: The absolute or relative path to the original .nc file.
+	:type source_path: str
+	:param target_path: The destination path where the modified .nc file will be saved.
+	:type target_path: str
+	:raises IOError: If the source file cannot be found or read.
+	:raises OSError: If there is a permissions issue or netCDF modification failure.
+	"""
 	#data = Dataset(pathto_raw_data_file,'r')
 	#data_dictionary = {}		 
 	#for key in data.variables.keys():
 	#	vals = data.variables[key][:]
 	#	print(key)
-	#	data_dictionary[key] = vals#np.where(vals == '--', np.nan, vals)    
+	#	data_dictionary[key] = vals#np.where(vals == '--', np.nan, vals)	
 
-   	desired_data_date = input("Enter the date of the desired raw data in iso format (YYYY-MM-DD) or leave blank to default to today's date: ")
+	desired_data_date = input("Enter the date of the desired raw data in iso format (YYYY-MM-DD) or leave blank to default to today's date: ")
 	if desired_data_date == "":
 		desired_data_date = date.today().isoformat()
 	file_suffix = input("Enter filename suffix or leave empty to default to 'test': ")
@@ -40,21 +40,32 @@ def run():
 	else:
 		pathto_raw_data_file = f'../L0_data/BayerRG8_{desired_data_date}_{file_suffix}.nc'		
 		pathto_L1_data_file = f'../L1_data/BayerRG8_{desired_data_date}_{file_suffix}.nc'
-
-    shutil.copy(pathto_raw_data_file, pathto_L1_data_file)# 1. Copy the original file to the new destination ("Save As" workaround)
+	baselinedate = np.datetime64(f'{desired_data_date}T00:00:00', 's')	
+	shutil.copy(pathto_raw_data_file, pathto_L1_data_file)# 1. Copy the original file to the new destination ("Save As" workaround)
 	gpsdate = desired_data_date.split("-")
-	gpsdate = "".join(gpsdate)	
-   	gpsfilename = [f for f in os.listdir(f'../gps_data/') if f.startswith(gpsdate)]
-    GPS_data = NMEA_decode.Run(filename=gpsfilename, path_to_file='../gps_data')
-   	with nc.Dataset(pathto_raw_data_file, 'a') as dataset:# 2. Open the newly copied file in append mode ('a') to modify it in place
-   		image_data = data.variables['raw_data'][:]
-   		image_time = data.variables['time'][:]
+	gpsdate_combined = "".join(gpsdate)	
+	gpsfilename = [f for f in os.listdir(f'../gps_data/') if f.startswith(gpsdate_combined)]
+	GPS_data = NMEA_decode.Run(filename=gpsfilename[0], path_to_file='../gps_data')
+	gpstime_list = [datetime(int(gpsdate[0]), int(gpsdate[1]), int(gpsdate[2]), t.hour, t.minute, t.second, t.microsecond) for t in GPS_data['GGA']['time']]
+	gpstime = np.array(gpstime_list, dtype='datetime64[s]')
+	gpslon = np.array(GPS_data['GGA']['lon'])
+	gpslat = np.array(GPS_data['GGA']['lat'])
+	gpsalt = np.array(GPS_data['GGA']['alt'])
+	with nc.Dataset(pathto_raw_data_file, 'a') as dataset:# 2. Open the newly copied file in append mode ('a') to modify it in place
+		image_data = dataset.variables['Raw_Signal'][:]
+		image_time = baselinedate + dataset.variables['time'][:].astype('timedelta64[s]')
 		dataset_length = len(image_time)
-		print(dataset_length)
 		h_pixel_length = len(image_data[0,:,0])
 		v_pixel_length = len(image_data[0,0,:])
-		print(h_pixel_length,v_pixel_length)
 		angles = np.deg2rad([0, 45, 90, 135])
+		s0 = np.full((dataset_length, h_pixel_length, v_pixel_length, 3), np.nan)
+		s1 = np.full((dataset_length, h_pixel_length, v_pixel_length, 3), np.nan)
+		s2 = np.full((dataset_length, h_pixel_length, v_pixel_length, 3), np.nan)
+		gps_lon = np.full((dataset_length),np.nan)
+		gps_lat = np.full((dataset_length),np.nan)
+		gps_alt = np.full((dataset_length),np.nan)
+		pan = np.full((dataset_length),np.nan)
+		tilt = np.full((dataset_length),np.nan)
 		for i1 in range(0,dataset_length):
 			# Demosaic the raw image into four polarization channels (0, 45, 90, 135 degrees)
 			# The 'pa.COLOR_PolarRGB' option handles the combined RGGB-polarization filter array.
@@ -64,48 +75,67 @@ def run():
 			image_list_bgr = [img_000_bgr, img_045_bgr, img_090_bgr, img_135_bgr]
 			image_list = np.sum(image_list_bgr,axis = 3)
 			img_stokes_bgr = pa.calcStokes(image_list_bgr, angles) 	
-			img_stokes_total = pa.calcStokes(image_list, angles) 	
-			# Convert the Stokes vector to Intensity, DoLP and AoLP
-			img_intensity_bgr = pa.cvtStokesToIntensity(img_stokes_bgr)
-			img_dolp_bgr = pa.cvtStokesToDoLP(img_stokes_bgr)
-			img_aolp_bgr = pa.cvtStokesToAoLP(img_stokes_bgr)	
-			img_dolp_total = pa.cvtStokesToDoLP(img_stokes_total)
-			img_aolp_total = pa.cvtStokesToAoLP(img_stokes_total)
-			# The results (s0, dolp, aolp) are full-resolution images. s0 represents the total intensity (a normal color image).
-			s0 = img_stokes_total[...,0]
-			s1 = img_stokes_total[...,1]
-			s2 = img_stokes_total[...,2]
-			q = s1 / s0
-			u = s2 / s0   
-
-			gps_index = np.where(GPS_data['GGA']['time'] == image_time)
-			sun_pos = get_position(image_time[i1], GPS_data['GGA']['longitude'], GPS_data['GGA']['latitude']) #Set position based on sun location
-			pan = np.degrees(sun_pos['azimuth'])
-			tilt = np.degrees(sun_pos['altitude']) 		
-
-
-
-
-        dataset.setncattr('history', 'Modified on 2026-07-14: Added new_dimension and new_variable.')# 5. Update and add global attributes
-        dataset.experiment_version = "v2.1"# You can use the setncattr method or direct assignment
-
-        # 3. Add a new dimension
-        # Checking if it exists first prevents errors if the script is run multiple times
-        if 'new_dimension' not in dataset.dimensions:
-            # createDimension(name, size). Use None for an unlimited dimension.
-            dataset.createDimension('new_dimension', 10) 
-            
-        # 4. Add a new variable using the new dimension
-        if 'new_variable' not in dataset.variables:
-            # createVariable(name, datatype, dimensions_tuple)
-            new_var = dataset.createVariable('new_variable', np.float32, ('new_dimension',))
-            
-            # Populate the variable with data
-            new_var[:] = np.linspace(0, 100, 10, dtype=np.float32)
-            
-            # Optional: Add metadata (attributes) to the specific variable
-            new_var.units = "degrees_celsius"
-            new_var.description = "Example variable added via Python script."
-            
-
-
+			s0[i1,...] = np.squeeze(img_stokes_bgr[...,0])
+			s1[i1,...] = np.squeeze(img_stokes_bgr[...,1])
+			s2[i1,...] = np.squeeze(img_stokes_bgr[...,2])
+			gps_index = np.where((gpstime == image_time[i1]))[0]
+			gps_lon[i1] = np.mean(gpslon[gps_index])
+			gps_lat[i1] = np.mean(gpslat[gps_index])
+			gps_alt[i1] = np.mean(gpsalt[gps_index])
+			sun_pos = get_position(image_time[i1], gps_lon[i1], gps_lat[i1]) #Set position based on sun location
+			pan[i1] = np.degrees(sun_pos['azimuth'])
+			tilt[i1] = np.degrees(sun_pos['altitude']) 		
+			
+		s_bins = np.array(['time', 'H_pixel', 'V_pixel'])
+		colors = ['blue', 'green', 'red']
+		i_clr = 0
+		for clr in colors:
+			I = dataset.createVariable(f'I_{clr}', 'f4', s_bins, zlib = True, complevel = 9)# createVariable(name, datatype, dimensions_tuple)			
+			I[:] = s0[...,i_clr]
+			I.short_name = 'total intensity'
+			I.units = 'DN'
+			I.long_name = f'{colors[i_clr]} light intensity signal in digital number (DN)'
+			I.ACVSNC_standard_name = f'Rad_Radiance_Remote_{colors[i_clr]}'   
+			Q = dataset.createVariable(f'Q_{clr}', 'f4', s_bins, zlib = True, complevel = 9)# createVariable(name, datatype, dimensions_tuple)			
+			Q[:] = s1[...,i_clr]
+			Q.short_name = 'horizontal-vertical intensity'
+			Q.units = 'DN'
+			Q.long_name = f'{colors[i_clr]} horizontal/vertical linear polarization'
+			Q.ACVSNC_standard_name = 'none' 
+			U = dataset.createVariable(f'U_{clr}', 'f4', s_bins, zlib = True, complevel = 9)# createVariable(name, datatype, dimensions_tuple)						
+			U[:] = s2[...,i_clr]
+			U.short_name = 'diagonal intensity'
+			U.units = 'DN'
+			U.long_name = f'{colors[i_clr]} +/-45 degree linear polarization'
+			U.ACVSNC_standard_name = 'none'
+			i_clr += 1
+		SA = dataset.createVariable('solar_azimuth', 'f4', s_bins, zlib = True, complevel = 9)# createVariable(name, datatype, dimensions_tuple)						
+		SA[:] = pan
+		SA.short_name = 'solar azimuth angle'
+		SA.units = 'degrees'
+		SA.long_name = 'Solar azimuth angle associated with sample position and time'
+		SA.ACVSNC_standard_name = 'none'
+		SA2 = dataset.createVariable('solar_altitude', 'f4', s_bins, zlib = True, complevel = 9)# createVariable(name, datatype, dimensions_tuple)						
+		SA2[:] = pan
+		SA2.short_name = 'solar altitude'
+		SA2.units = 'degrees'
+		SA2.long_name = 'Solar azimuth angle associated with sample position and time'
+		SA2.ACVSNC_standard_name = 'none'
+		lon = dataset.createVariable('GPS_longitude', 'f4', s_bins, zlib = True, complevel = 9)# createVariable(name, datatype, dimensions_tuple)						
+		lon[:] = gps_lon
+		lon.short_name = 'longitude'
+		lon.units = 'degrees'
+		lon.long_name = 'sample longitude derived from GPS in degrees W'
+		lon.ACVSNC_standard_name = 'none'
+		lat = dataset.createVariable('GPS_latitude', 'f4', s_bins, zlib = True, complevel = 9)# createVariable(name, datatype, dimensions_tuple)						
+		lat[:] = gps_lat
+		lat.short_name = 'latitude'
+		lat.units = 'degrees'
+		lat.long_name = 'sample latitude derived from GPS in degrees N'
+		lat.ACVSNC_standard_name = 'none'
+		alt = dataset.createVariable('GPS_altitude', 'f4', s_bins, zlib = True, complevel = 9)# createVariable(name, datatype, dimensions_tuple)						
+		alt[:] = gps_alt
+		alt.short_name = 'altitude'
+		alt.units = 'm'
+		alt.long_name = 'sample altitude in MASL'
+		alt.ACVSNC_standard_name = 'none'
